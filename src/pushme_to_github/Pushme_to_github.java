00fe2f49350086de6ddec50161dd85c885d5e4e5
Ishadya AP <115330980+git596@users.noreply.github.com>
2023-03-10 14:48:38 +0530

    push_project commit--> 03/10

---
+++ src/pushme_to_github/Pushme_to_github.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pushme_to_github;
+
+/**
+ *
+ * @author ishadya
+ */
+public class Pushme_to_github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey there I am using GitHub for the firts time");
+        // TODO code application logic here
+    }
+    
+}
